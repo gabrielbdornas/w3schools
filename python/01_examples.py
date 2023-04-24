1e69396ca2dc0e9970483a19441d84a7a8676727
Gabriel Dornas <gabrielbdornas@gmail.com>
2023-04-24 15:20:45 -0300

    Adiciona material do primeiro vídeo

---
+++ python/01_examples.py
@@ -0,0 +1,19 @@
+# Neste primeiro vídeo vimos:
+
+# 	- Uma breve introdução sobre python
+# 	  Link: https://www.w3schools.com/python/python_intro.asp
+
+# 	- Sintaxe Python e a importância da identação
+# 	  Link: https://www.w3schools.com/python/python_syntax.asp
+
+# 	- Comentário:
+# 	  Link: https://www.w3schools.com/python/python_syntax.asp	
+
+# This is a comment
+print('Hello, World!')
+
+my_first_variable = 5 # Minha primeira variável
+my_second_variable = 10
+result = my_first_variable + my_second_variable
+
+print(result)
